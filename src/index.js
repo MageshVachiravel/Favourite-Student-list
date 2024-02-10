d6c9721678336480c9668ed0f529e5d0cedddbdc
MageshVachiravel <mageshvachiravel@gmail.com>
2024-02-10 08:57:15 +0530

    feat: integrated tailwindCSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom/client"
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.querySelector("#root"))
 
 
 root.render(
-  <h1>Hello</h1>
+  <h1 className="bg-red-500">Hello</h1>
 )
